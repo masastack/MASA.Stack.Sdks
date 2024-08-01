92a5a9fed9961689adbe8f7c6b461ef51cb504d9
wzh425 <wuzihao425@outlook.com>
2024-08-01 16:28:52 +0800

    feat: ThirdPartyIdpService Add GetSelectAsync

---
+++ src/BuildingBlocks/Auth/Masa.BuildingBlocks.StackSdks.Auth.Contracts/Model/ThirdPartyIdpSelectModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Masa.BuildingBlocks.StackSdks.Auth.Contracts.Model;
+
+public class ThirdPartyIdpSelectModel
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string DisplayName { get; set; } = string.Empty;
+
+    public string ClientId { get; set; } = string.Empty;
+
+    public string ClientSecret { get; set; } = string.Empty;
+
+    public string Icon { get; set; } = string.Empty;
+
+    public AuthenticationTypes AuthenticationType { get; set; }
+}
