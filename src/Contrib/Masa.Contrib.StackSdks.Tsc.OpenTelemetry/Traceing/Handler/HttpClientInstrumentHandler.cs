58a302ee1eb16d5dc91a72140900cdbb52928ffe
qinyouzeng <qinyouzeng@masastack.com>
2026-07-01 17:09:43 +0800

    feat: httpclient 添加form表单记录支持

--- src/Contrib/Masa.Contrib.StackSdks.Tsc.OpenTelemetry/Traceing/Handler/HttpClientInstrumentHandler.cs
+++ src/Contrib/Masa.Contrib.StackSdks.Tsc.OpenTelemetry/Traceing/Handler/HttpClientInstrumentHandler.cs
@@ -113,16 +113,26 @@ internal class HttpClientInstrumentHandler : ExceptionHandler
 
     private static void LogFormFileContent(string source, string key, string fileName, Stream fileStream)
     {
+        var logger = OpenTelemetryInstrumentationOptions.Logger;
         (long length, string? content) = fileStream.ReadAsBase64();
         if (length <= 0)
             return;
+
+        using var scope = logger?.BeginScope(new Dictionary<string, object?>
+        {
+            ["source"] = source,
+            ["form.key"] = key,
+            ["file.name"] = fileName,
+            ["file.length"] = length
+        });
+
         if (length - OpenTelemetryInstrumentationOptions.MaxBodySize > 0)
         {
-            OpenTelemetryInstrumentationOptions.Logger?.LogInformation("[{Source}] Multipart form file content exceeded max limit. key: {Key}, fileName: {FileName}, length: {Length}, max: {MaxBodySize}", source, key, fileName, length, OpenTelemetryInstrumentationOptions.MaxBodySize);
+            logger?.LogInformation("file content exceeded max limit. max: {MaxBodySize}", OpenTelemetryInstrumentationOptions.MaxBodySize);
             return;
         }
 
-        OpenTelemetryInstrumentationOptions.Logger?.LogInformation("[{Source}] Multipart form file content(base64). key: {Key}, fileName: {FileName}, content: {Content}", source, key, fileName, content ?? string.Empty);
+        logger?.LogInformation("file content(base64): {Content}", content ?? string.Empty);
     }
 
     private static long? GetStreamLength(Stream stream)
